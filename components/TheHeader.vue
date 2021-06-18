e1e0af70d1c6f8adea595a52ec4747c149f413b5
jtotty <jt.totty1991@gmail.com>
2021-06-18 18:13:12 +0100

    Update index page.

---
+++ components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="py-6 flex items-center justify-between">
+    <nuxt-link :to="{ name: 'index' }" exact-active-class="text-blue-500">Jobs</nuxt-link>
+    <div class="flex item-center flex-wrap">
+      <span class="mx-4 font-bold">James</span>
+      <nuxt-link :to="{ name: 'login' }" exact-active-class="text-blue-500" class="mx-4">Login</nuxt-link>
+      <a href="" class="mx-4">Manage Listings</a>
+      <a href="" class="mx-4">Logut</a>
+      <a href="" class="ml-4">Post a Job</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
